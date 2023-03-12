a071450abad127bbc5cf72c2d1f68ad5b3c50b32
nb557 <>
2023-03-12 11:36:56 +0300

    Fix videocdn

--- online_mod.js
+++ online_mod.js
@@ -1,4 +1,4 @@
-//10.03.2023 - Fix kinobase
+//12.03.2023 - Fix videocdn
 
 (function () {
   'use strict';
@@ -165,50 +165,57 @@
       if (movie) {
         get_links_wait = true;
         var src = 'https:' + movie.iframe_src;
-        network.clear();
-        network.timeout(20000);
-        network.silent(src, function (raw) {
-          get_links_wait = false;
-          component.render().find('.broadcast__scan').remove();
-          var math = raw.replace(/\n/g, '').match(/id="files" value="(.*?)"/);
+        var meta = $('head meta[name="referrer"]');
+        var referrer = meta.attr('content') || 'never';
+        meta.attr('content', 'origin');
+        try {
+          network.clear();
+          network.timeout(20000);
+          network.silent(src, function (raw) {
+            get_links_wait = false;
+            component.render().find('.broadcast__scan').remove();
+            var math = raw.replace(/\n/g, '').match(/id="files" value="(.*?)"/);
 
-          if (!math) {
-            math = raw.replace(/\n/g, '').match(/id="files" value='(.*?)'/);
-          }
+            if (!math) {
+              math = raw.replace(/\n/g, '').match(/id="files" value='(.*?)'/);
+            }
 
-          if (math) {
-            var text = document.createElement("textarea");
-            text.innerHTML = math[1];
-            var json = Lampa.Arrays.decodeJson(text.value, {});
+            if (math) {
+              var text = document.createElement("textarea");
+              text.innerHTML = math[1];
+              var json = Lampa.Arrays.decodeJson(text.value, {});
 
-            for (var i in json) {
-              if (0 === i - 0) {
-                continue;
-              }
+              for (var i in json) {
+                if (0 === i - 0) {
+                  continue;
+                }
 
-              extract[i] = {
-                json: _typeof(json[i]) === 'object' ? json[i] : Lampa.Arrays.decodeJson(json[i], {}),
-                items: extractItems(json[i])
-              };
+                extract[i] = {
+                  json: _typeof(json[i]) === 'object' ? json[i] : Lampa.Arrays.decodeJson(json[i], {}),
+                  items: extractItems(json[i])
+                };
 
-              for (var a in extract[i].json) {
-                var elem = extract[i].json[a];
+                for (var a in extract[i].json) {
+                  var elem = extract[i].json[a];
 
-                if (elem.folder) {
-                  for (var f in elem.folder) {
-                    var folder = elem.folder[f];
-                    folder.items = extractItems(folder.file);
-                  }
-                } else elem.items = extractItems(elem.file);
+                  if (elem.folder) {
+                    for (var f in elem.folder) {
+                      var folder = elem.folder[f];
+                      folder.items = extractItems(folder.file);
+                    }
+                  } else elem.items = extractItems(elem.file);
+                }
               }
             }
-          }
-        }, function () {
-          get_links_wait = false;
-          component.render().find('.broadcast__scan').remove();
-        }, false, {
-          dataType: 'text'
-        });
+          }, function () {
+            get_links_wait = false;
+            component.render().find('.broadcast__scan').remove();
+          }, false, {
+            dataType: 'text'
+          });
+        } finally {
+          meta.attr('content', referrer);
+        }
       }
     }
     /**
@@ -4188,50 +4195,57 @@
       if (movie) {
         get_links_wait = true;
         var src = movie.iframe_src;
-        network.clear();
-        network.timeout(20000);
-        network.silent(src, function (raw) {
-          get_links_wait = false;
-          component.render().find('.broadcast__scan').remove();
-          var math = raw.replace(/\n/g, '').match(/id="files" value="(.*?)"/);
+        var meta = $('head meta[name="referrer"]');
+        var referrer = meta.attr('content') || 'never';
+        meta.attr('content', 'origin');
+        try {
+          network.clear();
+          network.timeout(20000);
+          network.silent(src, function (raw) {
+            get_links_wait = false;
+            component.render().find('.broadcast__scan').remove();
+            var math = raw.replace(/\n/g, '').match(/id="files" value="(.*?)"/);
 
-          if (!math) {
-            math = raw.replace(/\n/g, '').match(/id="files" value='(.*?)'/);
-          }
+            if (!math) {
+              math = raw.replace(/\n/g, '').match(/id="files" value='(.*?)'/);
+            }
 
-          if (math) {
-            var text = document.createElement("textarea");
-            text.innerHTML = math[1];
-            var json = Lampa.Arrays.decodeJson(text.value, {});
+            if (math) {
+              var text = document.createElement("textarea");
+              text.innerHTML = math[1];
+              var json = Lampa.Arrays.decodeJson(text.value, {});
 
-            for (var i in json) {
-              if (0 === i - 0) {
-                continue;
-              }
+              for (var i in json) {
+                if (0 === i - 0) {
+                  continue;
+                }
 
-              extract[i] = {
-                json: _typeof(json[i]) === 'object' ? json[i] : Lampa.Arrays.decodeJson(json[i], {}),
-                items: extractItems(json[i])
-              };
+                extract[i] = {
+                  json: _typeof(json[i]) === 'object' ? json[i] : Lampa.Arrays.decodeJson(json[i], {}),
+                  items: extractItems(json[i])
+                };
 
-              for (var a in extract[i].json) {
-                var elem = extract[i].json[a];
+                for (var a in extract[i].json) {
+                  var elem = extract[i].json[a];
 
-                if (elem.folder) {
-                  for (var f in elem.folder) {
-                    var folder = elem.folder[f];
-                    folder.items = extractItems(folder.file);
-                  }
-                } else elem.items = extractItems(elem.file);
+                  if (elem.folder) {
+                    for (var f in elem.folder) {
+                      var folder = elem.folder[f];
+                      folder.items = extractItems(folder.file);
+                    }
+                  } else elem.items = extractItems(elem.file);
+                }
               }
             }
-          }
-        }, function () {
-          get_links_wait = false;
-          component.render().find('.broadcast__scan').remove();
-        }, false, {
-          dataType: 'text'
-        });
+          }, function () {
+            get_links_wait = false;
+            component.render().find('.broadcast__scan').remove();
+          }, false, {
+            dataType: 'text'
+          });
+        } finally {
+          meta.attr('content', referrer);
+        }
       }
     }
     /**
@@ -5623,7 +5637,7 @@
     Lampa.Template.add('online_mod_folder', "<div class=\"online selector\">\n        <div class=\"online__body\">\n            <div style=\"position: absolute;left: 0;top: -0.3em;width: 2.4em;height: 2.4em\">\n                <svg style=\"height: 2.4em; width:  2.4em;\" viewBox=\"0 0 128 112\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n                    <rect y=\"20\" width=\"128\" height=\"92\" rx=\"13\" fill=\"white\"/>\n                    <path d=\"M29.9963 8H98.0037C96.0446 3.3021 91.4079 0 86 0H42C36.5921 0 31.9555 3.3021 29.9963 8Z\" fill=\"white\" fill-opacity=\"0.23\"/>\n                    <rect x=\"11\" y=\"8\" width=\"106\" height=\"76\" rx=\"13\" fill=\"white\" fill-opacity=\"0.51\"/>\n                </svg>\n            </div>\n            <div class=\"online__title\" style=\"padding-left: 2.1em;\">{title}</div>\n            <div class=\"online__quality\" style=\"padding-left: 3.4em;\">{quality}{info}</div>\n        </div>\n    </div>");
   }
 
-  var button = "<div class=\"full-start__button selector view--online_mod\" data-subtitle=\"online_mod 10.03.2023\">\n    <svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns:svgjs=\"http://svgjs.com/svgjs\" version=\"1.1\" width=\"512\" height=\"512\" x=\"0\" y=\"0\" viewBox=\"0 0 244 260\" style=\"enable-background:new 0 0 512 512\" xml:space=\"preserve\" class=\"\">\n    <g xmlns=\"http://www.w3.org/2000/svg\">\n        <path d=\"M242,88v170H10V88h41l-38,38h37.1l38-38h38.4l-38,38h38.4l38-38h38.3l-38,38H204L242,88L242,88z M228.9,2l8,37.7l0,0 L191.2,10L228.9,2z M160.6,56l-45.8-29.7l38-8.1l45.8,29.7L160.6,56z M84.5,72.1L38.8,42.4l38-8.1l45.8,29.7L84.5,72.1z M10,88 L2,50.2L47.8,80L10,88z\" fill=\"currentColor\"/>\n    </g></svg>\n\n    <span>#{online_mod_title}</span>\n    </div>"; // нужна заглушка, а то при страте лампы говорит пусто
+  var button = "<div class=\"full-start__button selector view--online_mod\" data-subtitle=\"online_mod 12.03.2023\">\n    <svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns:svgjs=\"http://svgjs.com/svgjs\" version=\"1.1\" width=\"512\" height=\"512\" x=\"0\" y=\"0\" viewBox=\"0 0 244 260\" style=\"enable-background:new 0 0 512 512\" xml:space=\"preserve\" class=\"\">\n    <g xmlns=\"http://www.w3.org/2000/svg\">\n        <path d=\"M242,88v170H10V88h41l-38,38h37.1l38-38h38.4l-38,38h38.4l38-38h38.3l-38,38H204L242,88L242,88z M228.9,2l8,37.7l0,0 L191.2,10L228.9,2z M160.6,56l-45.8-29.7l38-8.1l45.8,29.7L160.6,56z M84.5,72.1L38.8,42.4l38-8.1l45.8,29.7L84.5,72.1z M10,88 L2,50.2L47.8,80L10,88z\" fill=\"currentColor\"/>\n    </g></svg>\n\n    <span>#{online_mod_title}</span>\n    </div>"; // нужна заглушка, а то при страте лампы говорит пусто
 
   Lampa.Component.add('online_mod', component); //то же самое
 
