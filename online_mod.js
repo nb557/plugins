93835505e7a3f61aa5213bcb8ca48381df74216d
nb557 <>
2023-05-20 13:16:31 +0300

    Fix kinobase

--- online_mod.js
+++ online_mod.js
@@ -1,4 +1,4 @@
-//18.05.2023 - Fix kinobase
+//20.05.2023 - Fix kinobase
 
 (function () {
   'use strict';
@@ -2088,24 +2088,25 @@
           data_url = Lampa.Utils.addUrlComponent(data_url, "_=" + Math.floor(Date.now() / 1e3));
           network.clear();
           network.timeout(1000 * 10);
-          network["native"](embed + data_url, function (user_data_script) {
-            var user_data;
+          network["native"](embed + data_url, function (vod_script) {
+            var vod_url;
 
-            try {
-              user_data = user_data_script && eval(user_data_script.replace(/\$\[[^\]]*\]/, 'return (function(x,y,z){return y;})'));
-            } catch (e) {}
+            (function () {
+              var XMLHttpRequest = function () {
+                this.open = function (method, url) {
+                  vod_url = url;
+                };
+                this.send = function () {};
+              };
+              try {
+                eval(vod_script);
+              } catch (e) {}
+            })();
 
-            if (user_data && user_data.st && user_data.e) {
-              var file_url = "vod/" + select_id;
-              file_url = Lampa.Utils.addUrlComponent(file_url, "identifier=" + identifier);
-              file_url = Lampa.Utils.addUrlComponent(file_url, "player_type=new");
-              file_url = Lampa.Utils.addUrlComponent(file_url, "file_type=" + (prefer_mp4 ? "mp4" : "hls"));
-              file_url = Lampa.Utils.addUrlComponent(file_url, "st=" + user_data.st);
-              file_url = Lampa.Utils.addUrlComponent(file_url, "e=" + user_data.e);
-              file_url = Lampa.Utils.addUrlComponent(file_url, "_=" + Date.now());
+            if (vod_url) {
               network.clear();
               network.timeout(1000 * 10);
-              network["native"](embed + file_url, function (files) {
+              network["native"](embed + vod_url, function (files) {
                 component.loading(false);
                 extractData(files, str);
                 filter();
@@ -5882,7 +5883,7 @@
     Lampa.Template.add('online_mod_folder', "<div class=\"online selector\">\n        <div class=\"online__body\">\n            <div style=\"position: absolute;left: 0;top: -0.3em;width: 2.4em;height: 2.4em\">\n                <svg style=\"height: 2.4em; width:  2.4em;\" viewBox=\"0 0 128 112\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n                    <rect y=\"20\" width=\"128\" height=\"92\" rx=\"13\" fill=\"white\"/>\n                    <path d=\"M29.9963 8H98.0037C96.0446 3.3021 91.4079 0 86 0H42C36.5921 0 31.9555 3.3021 29.9963 8Z\" fill=\"white\" fill-opacity=\"0.23\"/>\n                    <rect x=\"11\" y=\"8\" width=\"106\" height=\"76\" rx=\"13\" fill=\"white\" fill-opacity=\"0.51\"/>\n                </svg>\n            </div>\n            <div class=\"online__title\" style=\"padding-left: 2.1em;\">{title}</div>\n            <div class=\"online__quality\" style=\"padding-left: 3.4em;\">{quality}{info}</div>\n        </div>\n    </div>");
   }
 
-  var button = "<div class=\"full-start__button selector view--online_mod\" data-subtitle=\"online_mod 18.05.2023\">\n    <svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns:svgjs=\"http://svgjs.com/svgjs\" version=\"1.1\" width=\"512\" height=\"512\" x=\"0\" y=\"0\" viewBox=\"0 0 244 260\" style=\"enable-background:new 0 0 512 512\" xml:space=\"preserve\" class=\"\">\n    <g xmlns=\"http://www.w3.org/2000/svg\">\n        <path d=\"M242,88v170H10V88h41l-38,38h37.1l38-38h38.4l-38,38h38.4l38-38h38.3l-38,38H204L242,88L242,88z M228.9,2l8,37.7l0,0 L191.2,10L228.9,2z M160.6,56l-45.8-29.7l38-8.1l45.8,29.7L160.6,56z M84.5,72.1L38.8,42.4l38-8.1l45.8,29.7L84.5,72.1z M10,88 L2,50.2L47.8,80L10,88z\" fill=\"currentColor\"/>\n    </g></svg>\n\n    <span>#{online_mod_title}</span>\n    </div>"; // нужна заглушка, а то при страте лампы говорит пусто
+  var button = "<div class=\"full-start__button selector view--online_mod\" data-subtitle=\"online_mod 20.05.2023\">\n    <svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns:svgjs=\"http://svgjs.com/svgjs\" version=\"1.1\" width=\"512\" height=\"512\" x=\"0\" y=\"0\" viewBox=\"0 0 244 260\" style=\"enable-background:new 0 0 512 512\" xml:space=\"preserve\" class=\"\">\n    <g xmlns=\"http://www.w3.org/2000/svg\">\n        <path d=\"M242,88v170H10V88h41l-38,38h37.1l38-38h38.4l-38,38h38.4l38-38h38.3l-38,38H204L242,88L242,88z M228.9,2l8,37.7l0,0 L191.2,10L228.9,2z M160.6,56l-45.8-29.7l38-8.1l45.8,29.7L160.6,56z M84.5,72.1L38.8,42.4l38-8.1l45.8,29.7L84.5,72.1z M10,88 L2,50.2L47.8,80L10,88z\" fill=\"currentColor\"/>\n    </g></svg>\n\n    <span>#{online_mod_title}</span>\n    </div>"; // нужна заглушка, а то при страте лампы говорит пусто
 
   Lampa.Component.add('online_mod', component); //то же самое
 
