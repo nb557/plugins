35d40327230bee9916bbb620863c4c8522f3085b
nb557 <>
2023-07-10 22:11:33 +0300

    Fix iframe proxy

--- online_mod.js
+++ online_mod.js
@@ -1,4 +1,4 @@
-//06.07.2023 - Fix iframe proxy
+//10.07.2023 - Fix iframe proxy
 
 (function () {
     'use strict';
@@ -74,8 +74,7 @@
       var prefer_mp4 = Lampa.Storage.field('online_mod_prefer_mp4') === true;
       var prox = component.proxy('videocdn');
       var embed = prox + (prefer_http || prox ? 'http:' : 'https:') + '//videocdn.tv/api/';
-      //var iframe_proxy = !prox && Lampa.Storage.field('online_mod_iframe_proxy') === true && !window.location.protocol.startsWith('http');
-      var iframe_proxy = false;
+      var iframe_proxy = !prox && Lampa.Storage.field('online_mod_iframe_proxy') === true && Lampa.Storage.field('online_mod_alt_iframe_proxy') !== true && !window.location.protocol.startsWith('http');
       var filter_items = {};
       var choice = {
         season: 0,
@@ -276,7 +275,7 @@
 
           if (iframe_proxy) {
             component.proxyCall('GET', src, 20000, null, call_success, call_fail);
-          } else if (window.location.protocol !== 'http:') { 
+          } else if (window.location.protocol !== 'http:') {
             network.clear();
             network.timeout(20000);
             network.silent('https://cors.nb557.workers.dev/' + src, call_success, call_fail, false, {
@@ -5261,9 +5260,16 @@
 
         if (!proxyInitialized) {
           proxyInitialized = true;
-          var proxyOrigin = 'https://nb557.github.io';
+          var proxyOrigin = Lampa.Utils.protocol() + 'nb557.surge.sh';
+          var proxyUrl = proxyOrigin + '/proxy.html';
+
+          if (Lampa.Storage.field('online_mod_alt_iframe_proxy') === true) {
+            proxyOrigin = 'https://nb557.github.io';
+            proxyUrl = proxyOrigin + '/plugins/proxy.html';
+          }
+
           var proxyIframe = document.createElement('iframe');
-          proxyIframe.setAttribute('src', proxyOrigin + '/plugins/proxy.html');
+          proxyIframe.setAttribute('src', proxyUrl);
           proxyIframe.setAttribute('width', '0');
           proxyIframe.setAttribute('height', '0');
           proxyIframe.setAttribute('tabindex', '-1');
@@ -6068,7 +6074,7 @@
       Lampa.Template.add('online_mod_folder', "<div class=\"online selector\">\n        <div class=\"online__body\">\n            <div style=\"position: absolute;left: 0;top: -0.3em;width: 2.4em;height: 2.4em\">\n                <svg style=\"height: 2.4em; width:  2.4em;\" viewBox=\"0 0 128 112\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n                    <rect y=\"20\" width=\"128\" height=\"92\" rx=\"13\" fill=\"white\"/>\n                    <path d=\"M29.9963 8H98.0037C96.0446 3.3021 91.4079 0 86 0H42C36.5921 0 31.9555 3.3021 29.9963 8Z\" fill=\"white\" fill-opacity=\"0.23\"/>\n                    <rect x=\"11\" y=\"8\" width=\"106\" height=\"76\" rx=\"13\" fill=\"white\" fill-opacity=\"0.51\"/>\n                </svg>\n            </div>\n            <div class=\"online__title\" style=\"padding-left: 2.1em;\">{title}</div>\n            <div class=\"online__quality\" style=\"padding-left: 3.4em;\">{quality}{info}</div>\n        </div>\n    </div>");
     }
 
-    var button = "<div class=\"full-start__button selector view--online_mod\" data-subtitle=\"online_mod 06.07.2023\">\n    <svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns:svgjs=\"http://svgjs.com/svgjs\" version=\"1.1\" width=\"512\" height=\"512\" x=\"0\" y=\"0\" viewBox=\"0 0 244 260\" style=\"enable-background:new 0 0 512 512\" xml:space=\"preserve\" class=\"\">\n    <g xmlns=\"http://www.w3.org/2000/svg\">\n        <path d=\"M242,88v170H10V88h41l-38,38h37.1l38-38h38.4l-38,38h38.4l38-38h38.3l-38,38H204L242,88L242,88z M228.9,2l8,37.7l0,0 L191.2,10L228.9,2z M160.6,56l-45.8-29.7l38-8.1l45.8,29.7L160.6,56z M84.5,72.1L38.8,42.4l38-8.1l45.8,29.7L84.5,72.1z M10,88 L2,50.2L47.8,80L10,88z\" fill=\"currentColor\"/>\n    </g></svg>\n\n    <span>#{online_mod_title}</span>\n    </div>"; // нужна заглушка, а то при страте лампы говорит пусто
+    var button = "<div class=\"full-start__button selector view--online_mod\" data-subtitle=\"online_mod 10.07.2023\">\n    <svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns:svgjs=\"http://svgjs.com/svgjs\" version=\"1.1\" width=\"512\" height=\"512\" x=\"0\" y=\"0\" viewBox=\"0 0 244 260\" style=\"enable-background:new 0 0 512 512\" xml:space=\"preserve\" class=\"\">\n    <g xmlns=\"http://www.w3.org/2000/svg\">\n        <path d=\"M242,88v170H10V88h41l-38,38h37.1l38-38h38.4l-38,38h38.4l38-38h38.3l-38,38H204L242,88L242,88z M228.9,2l8,37.7l0,0 L191.2,10L228.9,2z M160.6,56l-45.8-29.7l38-8.1l45.8,29.7L160.6,56z M84.5,72.1L38.8,42.4l38-8.1l45.8,29.7L84.5,72.1z M10,88 L2,50.2L47.8,80L10,88z\" fill=\"currentColor\"/>\n    </g></svg>\n\n    <span>#{online_mod_title}</span>\n    </div>"; // нужна заглушка, а то при страте лампы говорит пусто
 
     Lampa.Component.add('online_mod', component); //то же самое
 
@@ -6250,6 +6256,7 @@
     Lampa.Storage.set('online_mod_proxy_cdnmovies', 'false');
     Lampa.Storage.set('online_mod_proxy_hdvb', 'false');
     Lampa.Storage.set('online_mod_proxy_kp', 'false');
+    Lampa.Storage.set('online_mod_alt_iframe_proxy', 'false');
     Lampa.Params.trigger('online_mod_iframe_proxy', true);
     Lampa.Params.trigger('online_mod_proxy_other', false);
     Lampa.Params.trigger('online_mod_proxy_videocdn', false);
