87e65b26b5d65e365102854fcacaa4a21ba1ee55
nb557 <>
2024-03-15 03:12:36 +0300

    apn

---
+++ deno_apn.js
@@ -0,0 +1,196 @@
+async function handle(request, connInfo) {
+    const corsHeaders = {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+      "Access-Control-Max-Age": "86400",
+    };
+
+    async function handleRequest(request, connInfo) {
+      const url = new URL(request.url);
+      let api = url.href.substring(url.origin.length + 1);
+      let ip;
+      let get_cookie;
+      let params = [];
+
+      if (api === "headers") {
+        let body = "";
+        request.headers.forEach((value, key) => body += key + " = " + value + "\n");
+        if (connInfo && connInfo.remoteAddr) {
+          body += "connInfo" + " = " + JSON.stringify(connInfo.remoteAddr) + "\n";
+        }
+        return new Response(body);
+      }
+
+      let next_param = true;
+      while (next_param) {
+        if (api.startsWith("ip")) {
+          let pos = api.indexOf("/");
+          if (pos !== -1) {
+            ip = api.substring(2, pos);
+            api = api.substring(pos + 1);
+          } else {
+            ip = api.substring(2);
+            api = "";
+          }
+        } else if (api.startsWith("get_cookie/")) {
+          get_cookie = true;
+          api = api.substring(11);
+        } else if (api.startsWith("param?")) {
+          let param;
+          let pos = api.indexOf("/");
+          if (pos !== -1) {
+            param = api.substring(6, pos);
+            api = api.substring(pos + 1);
+          } else {
+            param = api.substring(6);
+            api = "";
+          }
+          params.push(param.split("="));
+        } else {
+          next_param = false;
+        }
+      }
+      if (!ip) {
+        ip = connInfo && connInfo.remoteAddr && connInfo.remoteAddr.hostname || "";
+      }
+
+      if (!api || !/^https?:\/\/[^\/]/.test(api)) {
+        return new Response(null, {
+          status: 404,
+          statusText: "No Api Url",
+        });
+      }
+      const apiUrl = new URL(api);
+
+      // Rewrite request to point to API URL. This also makes the request mutable
+      // so you can add the correct Origin header to make the API server think
+      // that this request is not cross-site.
+      request = new Request(api, request);
+      request.headers.set("Origin", apiUrl.origin);
+      request.headers.set("Referer", apiUrl.origin + "/");
+      if (true) {
+        request.headers.delete("Sec-Fetch-Dest");
+        request.headers.delete("Sec-Fetch-Mode");
+        request.headers.delete("Sec-Fetch-Site");
+        request.headers.delete("Sec-Fetch-User");
+        request.headers.delete("Sec-CH-UA");
+        request.headers.delete("Sec-CH-UA-Mobile");
+        request.headers.delete("Sec-CH-UA-Platform");
+        request.headers.delete("Host");
+      }
+      if (true) {
+        request.headers.delete("X-Forwarded-For");
+        request.headers.delete("X-Forwarded-Proto");
+        //request.headers.delete("X-Real-IP");
+        //request.headers.delete("cf-connecting-ip");
+        request.headers.delete("cf-ipcountry");
+        request.headers.delete("cf-ray");
+        request.headers.delete("cf-visitor");
+      }
+      if (ip) {
+        request.headers.set("X-Forwarded-For", ip);
+        request.headers.set("X-Forwarded-Proto", "https");
+        request.headers.set("X-Real-IP", ip);
+        request.headers.set("cf-connecting-ip", ip);
+      }
+      if (apiUrl.hostname === "rezka.ag" || apiUrl.hostname === "hdrezka.ag" || apiUrl.hostname === "hdrezka.me" || apiUrl.hostname === "hdrezka.sh" || apiUrl.hostname === "hdrezka.cm" || apiUrl.hostname === "hdrezka.kim" || apiUrl.hostname === "hdrezka.la" || apiUrl.hostname === "rezka.pub" || apiUrl.hostname === "kinopub.me") {
+        request.headers.set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36");
+      }
+      if (apiUrl.hostname.endsWith(".svetacdn.in")) {
+        request.headers.set("Origin", "https://videocdn.tv");
+        request.headers.set("Referer", "https://videocdn.tv/");
+      }
+      if (apiUrl.hostname.endsWith("cdnmovies-stream.online") || apiUrl.hostname.endsWith("cdnmovies-hls-stream.online") || apiUrl.hostname.endsWith(".sarnage.cc")) {
+        request.headers.set("Origin", "https://cdnmovies.net");
+        request.headers.set("Referer", "https://cdnmovies.net/");
+      }
+      if (apiUrl.hostname.endsWith(".bazon.site")) {
+        request.headers.set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36");
+        request.headers.set("Origin", "https://bazon.cc");
+        request.headers.set("Referer", "https://bazon.cc/");
+      }
+      if (["kodikapi.com", "kodik.biz", "kodik.info"].indexOf(apiUrl.hostname) !== -1) {
+        request.headers.delete("Origin");
+        request.headers.delete("Referer");
+      }
+      if (apiUrl.hostname === "kinoplay.site" || apiUrl.hostname === "kinoplay1.site" || apiUrl.hostname === "kinoplay2.site") {
+        request.headers.set("Cookie", "invite=a246a3f46c82fe439a45c3dbbbb24ad5");
+      }
+      params.forEach(param => {
+        if (param[0]) {
+          request.headers.set(decodeURIComponent(param[0]), decodeURIComponent(param[1] || ""));
+        }
+      });
+      let response = await fetch(request);
+
+      // Recreate the response so you can modify the headers
+      response = new Response(response.body, response);
+
+      // Set CORS headers
+      response.headers.set("Access-Control-Allow-Origin", "*");
+
+      // Fix redirect relative URL
+      if (response.status >= 300 && response.status < 400) {
+        let target = response.headers.get("Location");
+        if (target && target.startsWith("/")) {
+          response.headers.set("Location", url.origin + "/" + apiUrl.origin + target);
+        }
+      }
+
+      // Append to/Add Vary header so browser will cache response correctly
+      response.headers.append("Vary", "Origin");
+
+      if (get_cookie) {
+        let json = {};
+        json.cookie = response.headers.getSetCookie();
+        response = new Response(JSON.stringify(json), response);
+        response.headers.delete("Set-Cookie");
+        response.headers.set("Content-Type", "application/json; charset=utf-8");
+      }
+      return response;
+    }
+
+    async function handleOptions(request, connInfo) {
+      if (
+        request.headers.get("Origin") !== null &&
+        request.headers.get("Access-Control-Request-Method") !== null &&
+        request.headers.get("Access-Control-Request-Headers") !== null
+      ) {
+        // Handle CORS preflight requests.
+        return new Response(null, {
+          headers: {
+            ...corsHeaders,
+            "Access-Control-Allow-Headers": request.headers.get(
+              "Access-Control-Request-Headers"
+            ),
+          },
+        });
+      } else {
+        // Handle standard OPTIONS request.
+        return new Response(null, {
+          headers: {
+            Allow: "GET, HEAD, POST, OPTIONS",
+          },
+        });
+      }
+    }
+
+    if (request.method === "OPTIONS") {
+      // Handle CORS preflight requests
+      return handleOptions(request, connInfo);
+    } else if (
+      request.method === "GET" ||
+      request.method === "HEAD" ||
+      request.method === "POST"
+    ) {
+      // Handle requests to the API server
+      return handleRequest(request, connInfo);
+    } else {
+      return new Response(null, {
+        status: 405,
+        statusText: "Method Not Allowed",
+      });
+    }
+}
+
+Deno.serve(handle);
