8103484b19debcd39f15c288f4ffad8eb8b4f346
nb557 <>
2024-12-07 19:44:39 +0300

    etor copy

---
+++ etor.js
@@ -0,0 +1,10 @@
+(function () {
+	'use strict';
+
+	//Не нужно переписывать весь этот код, не делайте так!
+	//for Lampa.
+	window.lampa_settings.torrents_use = true;
+	window.lampa_settings.demo = false;
+	window.lampa_settings.read_only = false;
+
+})();
