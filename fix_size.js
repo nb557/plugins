4aa3be060a576e09a00cc28e6ba37f2c2478d421
nb557 <>
2025-03-27 22:48:32 +0300

    fix_size: card "more" size

--- fix_size.js
+++ fix_size.js
@@ -51,6 +51,31 @@
             item.insertAfter(e.body.find('[data-name="interface_size"]'));
           }
         });
+        var css = $('style#fix_size_css');
+
+        if (!css.length) {
+          css = $('<style type="text/css" id="fix_size_css"></style>');
+          css.appendTo('head');
+        }
+
+        css.html('.card--category { width: 16em !important }');
+        var platform_screen = Lampa.Platform.screen;
+
+        Lampa.Platform.screen = function (need) {
+          if (need === 'tv') {
+            try {
+              var stack = new Error().stack.split('\n');
+              var offset = stack[0] === 'Error' ? 1 : 0;
+
+              if (/^( *at +new +)?create\$i/.test(stack[1 + offset]) && /^( *at +)?component(\/this)?\.append/.test(stack[2 + offset])) {
+                return false;
+              }
+            } catch (e) {}
+          }
+
+          return platform_screen(need);
+        };
+
         var layer_update = Lampa.Layer.update;
 
         Lampa.Layer.update = function (where) {
