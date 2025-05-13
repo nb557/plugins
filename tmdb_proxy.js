070feeddb6c4b9e089e3bd5a441e8e4f2f2206f2
nb557 <>
2025-05-14 00:45:37 +0300

    TMDB-Proxy

---
+++ tmdb_proxy.js
@@ -0,0 +1,30 @@
+(function () {
+    'use strict';
+
+    function filter(u) {
+      return u.replace(/\/\/+/g, '/');
+    }
+
+    function proxyLink(u) {
+      var proxy = Lampa.Storage.field('proxy_tmdb') ? Lampa.Utils.protocol() + 'keuytkxq.deploy.cx/' : '';
+      return proxy + u;
+    }
+
+    Lampa.TMDB.image = function (url) {
+      var base = Lampa.Utils.protocol() + filter('image.tmdb.org/' + url);
+      return proxyLink(base);
+    };
+
+    Lampa.TMDB.api = function (url) {
+      var base = Lampa.Utils.protocol() + filter('api.themoviedb.org/3/' + url);
+      return proxyLink(base);
+    };
+
+    Lampa.Settings.listener.follow('open', function (e) {
+      if (e.name == 'tmdb') {
+        e.body.find('[data-parent="proxy"]').remove();
+      }
+    });
+    console.log('TMDB-Proxy', 'nb557, enabled:', Lampa.Storage.field('proxy_tmdb'));
+
+})();
