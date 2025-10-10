453c9ac760b2fa1896d40133eefb7104baa96c95
nb557 <>
2025-10-10 11:33:19 +0300

    lg_update fix css

--- lg_update.js
+++ lg_update.js
@@ -3,12 +3,25 @@
 
     function startPlugin() {
       window.lg_update_plugin = true;
+      var src = 'http://lampa.mx/';
       var network = new Lampa.Reguest();
       network.clear();
       network.timeout(1000 * 30);
-      network.silent('http://lampa.mx/app.min.js', function (app) {
-        window.localStorage.setItem('app.js', app);
-        Lampa.Noty.show('LG Update: OK. Reboot Lampa');
+      network.silent(src + 'app.min.js?v=lg_update', function (app) {
+        var old = window.localStorage.getItem('app.js');
+
+        if (old != app) {
+          window.localStorage.setItem('app.js', app);
+          Lampa.Noty.show('LG Update: OK. Reboot Lampa');
+        }
+
+        var old_css = $('link[href="css/app.css"]');
+
+        if (old_css.length) {
+          Lampa.Utils.putStyle([src + 'css/app.css?v=lg_update'], function () {
+            old_css.remove();
+          }, function () {});
+        }
       }, function (a, c) {
         Lampa.Noty.show('LG Update: ' + network.errorDecode(a, c));
       }, false, {
